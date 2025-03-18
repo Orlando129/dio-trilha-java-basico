5079ce339ed7e87f07893b3eb35293fd4565a54e
Orlando <orlandotelles129@gmail.com>
2025-03-18 18:26:22 -0300

    Primeiro commit conta bancaria

--- ContaBanco/src/main/java/org/example/ContaTerminal.java
+++ ContaBanco/src/main/java/org/example/ContaTerminal.java
@@ -1,7 +1,31 @@
 package org.example;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hellow world!");
+        Locale.setDefault(Locale.US);
+        int Numero;
+        String Agencia;
+        String NomeCliente;
+        double Saldo;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o numero da Conta: ");
+        Numero = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Por favor, digite o numero da Agencia: ");
+        Agencia = sc.next();
+        System.out.print("Por favor, digite o seu Nome: ");
+        NomeCliente = sc.nextLine();
+        System.out.print("Por favor, digite o seu Saldo: ");
+        Saldo = sc.nextDouble();
+
+        System.out.println("Ola "+NomeCliente+" ,obrigado por criar uma conta em nosso banco, sua agencia e "+Agencia+
+                " conta "+ Numero+ "\n e seu saldo "+Saldo+" ja esta disponivel para saque.");
+
+        sc.close();
     }
 }
